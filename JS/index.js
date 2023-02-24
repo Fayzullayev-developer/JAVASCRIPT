bf533e0e15e8146f63fcc9c9353939861cbd27d2
Fayzullayev-developer <fayzullayevsurat8@gmail.com>
2023-02-25 00:09:56 +0500

    variables

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+var tel = 998900147383;   /* number */
+var first_name = 'Surat'; /* string */
+var hello ;               /* undefined */
+var futbolist = true;     /* boolean */
+var box = null;           /* object */
+
+
+console.log(tel);         /* number */
+console.log(first_name);  /* string */
+console.log(hello);       /* undefined */
+console.log(futbolist);   /* boolean */
+console.log(box);         /* object */
